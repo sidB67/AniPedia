1fc0f8251cced895ab5dfd194ffd2151488ceffe
Sidharth Bahl <sidbahl67@gmail.com>
2022-04-14 23:29:24 +0530

    bugs

--- lib/UI/homescreen/homepage.dart
+++ lib/UI/homescreen/homepage.dart
@@ -30,11 +30,49 @@ class HomePage extends StatelessWidget {
                   itemCount: 6,
                   itemBuilder: (context, idx) {
                     return Container(
-                      margin: EdgeInsets.only(right: 20),
+                      margin: EdgeInsets.only(right: 20, bottom: 5),
                       decoration: BoxDecoration(
-                          color: Colors.red,
-                          borderRadius: BorderRadius.circular(20)),
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20),
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.black,
+                                blurRadius: 2,
+                                offset: Offset(3.0, 4.0)),
+                          ]),
                       width: 220,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          //For image of the anime
+                          Container(
+                            width: double.infinity,
+                            height: 180,
+                            child: ClipRRect(
+                              borderRadius: const BorderRadius.only(
+                                  topLeft: Radius.circular(20),
+                                  topRight: Radius.circular(20)),
+                              child: Image.network(
+                                'https://cdn.myanimelist.net/images/anime/1493/116732l.jpg',
+                                fit: BoxFit.fill,
+                              ),
+                            ),
+                          ),
+                          //For title of anime
+                          Container(
+                            margin: const EdgeInsets.only(left: 10, top: 7),
+                            width: 200,
+                            child: Text(
+                              '${idx + 1}. Naruto Shippuden',
+                              style: const TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w700),
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          )
+                        ],
+                      ),
                     );
                   }),
             ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:anipedia/providers/anime_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'UI/homescreen/homepage.dart';
 
 void main() {
@@ -11,10 +13,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(),
-      home: const HomePage(),
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (context) => AnimeProvider())],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(),
+        home: const HomePage(),
+      ),
     );
   }
 }
